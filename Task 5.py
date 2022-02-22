03295e2ed0f05767d1a47258280f80805ce17d5d
Jeremy Chow <100083686+jchow257@users.noreply.github.com>
2022-02-22 18:38:47 +0800

    Add files via upload

--- Task 5.py
+++ Task 5.py
@@ -1,1 +1,13 @@
-print("testing")
+import requests
+url = 'https://unsplash.com/'
+x = requests.get(url)
+print(x.text)
+print("Status code:")
+print("\t*", x.status_code)
+
+h = requests.head(url)
+print("Header: ")
+print("Top of the header")
+for y in h.headers:
+    print("\t", y, ":", h.headers[y])
+print("Bottom of the header")
